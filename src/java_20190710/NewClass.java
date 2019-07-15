a42fe1ebd554964987c1c9bde5c10f9ede9a4e1f
FAT00006 <FAT00006@IC0-W-D00868.ssg.global>
2019-07-15 11:37:51 +0900

    20190715

--- src/java_20190710/NewClass.java
+++ src/java_20190710/NewClass.java
@@ -1,5 +1,27 @@
 package java_20190710;
 
-public class NewClass {
+import java.util.Arrays;
 
+public class NewClass {
+	public static void main(String args[]) {
+
+		int[] evenGroup = new int[4];
+		int[] oddGroup = new int[5];
+
+		int evenNumber = 0;
+		int oddNumber = 0;
+
+		for (int i = 1; i < 10; i++) {
+			if (i % 2 == 0) {
+				evenGroup[evenNumber] = i;
+				evenNumber++;
+			} else {
+				oddGroup[oddNumber] = i;
+				oddNumber++;
+			}
+		
+			System.out.print(Arrays.toString(evenGroup));
+			System.out.print(Arrays.toString(oddGroup));
+		}
+	}
 }
